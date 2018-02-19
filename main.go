08d3dd3b1e47487c4489bc5307bc126aa0b29abf
spddl <spddlhh@gmail.com>
2018-02-19 22:36:48 +0100

    .

--- main.go
+++ main.go
@@ -1,46 +1,58 @@
 package main
 
 import (
-	"os"
-	"fmt"
-	"time"
-	"strconv"
 	"encoding/json"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+	"time"
 
-	"github.com/jhoonb/archivex"
 	"github.com/jasonlvhit/gocron"
+	"github.com/jhoonb/archivex"
 )
 
-type Configuration struct {
-	Backup   string
-	Interval int
+type configuration struct {
+	Path     string `json:"path"`
+	Interval int    `json:"interval"`
+	Files    []struct {
+		Name string `json:"name"`
+		Path string `json:"path"`
+	} `json:"files"`
 }
 
 func main() {
 
-	file, _ := os.Open("conf.json")
+	file, _ := os.Open("config.json")
 	decoder := json.NewDecoder(file)
-	config := Configuration{}
+	config := configuration{}
 	err := decoder.Decode(&config)
 
 	if err != nil {
-		fmt.Println("[Backup] Unable to load conf.json, are you sure it's present? Error:", err.Error())
+		fmt.Println("[Backup] Unable to load config.json, are you sure it's present? Error:", err.Error())
 		os.Exit(1)
 	}
 
-	fmt.Println("[Backup] Started up... Preparing to archive:", config.Backup, "Every:", config.Interval, "seconds.")
-
-	backup := func() {
-		zip := new(archivex.ZipFile)
-		zipName := "world@" + strconv.Itoa(int(time.Now().Unix()))
-		zip.Create(zipName)
-		zip.AddAll(config.Backup, true)
-		zip.Close()
-		fmt.Println("[Backup] Successfully archived:", config.Backup, "Id:", zipName)
+	names := make([]string, 0)
+	for _, json := range config.Files {
+		names = append(names, json.Name)
 	}
 
+	fmt.Println("[Backup] Started up... Every:", config.Interval, "seconds. Preparing to archive:", strings.Join(names[:], ", "))
+
+	backup := func() {
+		for _, json := range config.Files {
+			zip := new(archivex.ZipFile)
+			zipName := config.Path + json.Name + "@" + strconv.Itoa(int(time.Now().Unix()))
+			zip.Create(zipName)
+			zip.AddAll(json.Path, true)
+			zip.Close()
+			fmt.Println("[Backup] Successfully archived:", zipName+".zip")
+		}
+	}
+
+	backup()
 	s := gocron.NewScheduler()
 	s.Every(uint64(config.Interval)).Seconds().Do(backup)
 	<-s.Start()
-
 }
